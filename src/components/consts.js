f73ea1d4e798081e20e7b82a78a4ba30bae2706f
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2023-09-20 13:20:21 +0500

    added portfolio site

--- src/components/consts.js
+++ src/components/consts.js
@@ -1,104 +1,110 @@
-export const navLink =[
-    {
-        id: 'home',
-        title: 'Home'
-    },
-    {
-        id: 'about',
-        title: 'About'
-    },
-    {
-        id: 'skills',
-        title: 'Skills'
-    },
-    {
-        id: 'portfolio',
-        title: 'Portfolio'
-    },
-]
+export const navLink = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+];
 
 export const stats = [
-    {
-        id: 1,
-        title:  'Foydalanuvchi Faol',
-        value: '9300+'
-    },
-    {
-        id: 2,
-        title:  'Kompaniya homiyligida',
-        value: '100+'
-    },
-    {
-        id: 3,
-        title:  'Tranzaksiya',
-        value: '110M+'
-    },
-]
+  {
+    id: 1,
+    title: "Foydalanuvchi Faol",
+    value: "9300+",
+  },
+  {
+    id: 2,
+    title: "Kompaniya homiyligida",
+    value: "100+",
+  },
+  {
+    id: 3,
+    title: "Tranzaksiya",
+    value: "110M+",
+  },
+];
 
-export const portfolio =[
-    {
-        id: 1,
-        title: 'Calculator',
-        url: 'https://react-calculator-qwasar.vercel.app/',
-        desc: 'Calculator made with ReactJS for Qwasar',
-    },
-    {
-        id: 2,
-        title: 'Shopping',
-        url: 'https://uzshop.vercel.app',
-        desc: 'Kids shopping website',
-    },
-    {
-        id: 3,
-        title: 'Light Lamp',
-        url: 'https://light-lamp.vercel.app',
-        desc: 'Simple Landing Page like Lamp',
-    },
-    {
-        id: 4,
-        title: 'Portfolio',
-        url: 'https://portfolio-000.vercel.app',
-        desc: 'Simple portfolio website made with html css',
-    },
-    {
-        id: 5,
-        title: 'MovieApp',
-        url: 'https://movieappuz.vercel.app/',
-        desc: 'Movie searching Site with API',
-    },
-    {
-        id: 6,
-        title: 'Girl Kate',
-        url: 'https://girl-kate.vercel.app/',
-        desc: 'Simple landing page with good design',
-    },
-    {
-        id: 7,
-        title: 'Filmy',
-        url: 'https://filmy-w.vercel.app/',
-        desc: 'FILMY - Panjara Ortida',
-    },
-]
+export const portfolio = [
+  {
+    id: 8,
+    title: "Yurak Amri Travel",
+    url: "https://yurak-amri-travel.uz/",
+    desc: "Yurak Amri Travel - Tour Company",
+  },
+  {
+    id: 7,
+    title: "Calculator",
+    url: "https://react-calculator-qwasar.vercel.app/",
+    desc: "Calculator made with ReactJS for Qwasar",
+  },
+  {
+    id: 6,
+    title: "Shopping",
+    url: "https://uzshop.vercel.app",
+    desc: "Kids shopping website",
+  },
+  {
+    id: 5,
+    title: "Light Lamp",
+    url: "https://light-lamp.vercel.app",
+    desc: "Simple Landing Page like Lamp",
+  },
+  {
+    id: 4,
+    title: "Portfolio",
+    url: "https://portfolio-000.vercel.app",
+    desc: "Simple portfolio website made with html css",
+  },
+  {
+    id: 3,
+    title: "MovieApp",
+    url: "https://movieappuz.vercel.app/",
+    desc: "Movie searching Site with API",
+  },
+  {
+    id: 2,
+    title: "Girl Kate",
+    url: "https://girl-kate.vercel.app/",
+    desc: "Simple landing page with good design",
+  },
+  {
+    id: 1,
+    title: "Filmy",
+    url: "https://filmy-w.vercel.app/",
+    desc: "FILMY - Panjara Ortida",
+  },
+];
 
-export const progres =[
-    {
-        id: 1,
-        title: 'HTML',
-        percent: '95'
-    },
-    {
-        id: 1,
-        title: 'CSS',
-        percent: '85'
-    },
-    {
-        id: 1,
-        title: 'JS',
-        percent: '60'
-    },
-    {
-        id: 1,
-        title: 'ReactJS',
-        percent: '60'
-    },
-]
+export const progres = [
+  {
+    id: 1,
+    title: "HTML",
+    percent: "95",
+  },
+  {
+    id: 1,
+    title: "CSS",
+    percent: "85",
+  },
+  {
+    id: 1,
+    title: "JS",
+    percent: "60",
+  },
+  {
+    id: 1,
+    title: "ReactJS",
+    percent: "60",
+  },
+];
